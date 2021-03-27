7201d030b3ec959fd7eeb46b2d78cc9a5c2f6ea7
Андрей Хан <81438426+AndrewKhan1983@users.noreply.github.com>
2021-03-27 12:31:35 +0300

    Create main.java

---
+++ Task_2.1/main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+       String name = "Мама";
+       String act = "Мыла";
+       String thing = "Раму";
+        System.out.println(name+act+thing);
+        System.out.println(act+name+thing);
+        System.out.println(thing+name+act);
+        System.out.println(name+thing+act);
+        System.out.println(act+thing+act);
+        System.out.println(thing+act+name);
+
+    }
+}
