a4e39e98e50cb48d091afb4fe00062e227f1f945
Андрей Хан <81438426+AndrewKhan1983@users.noreply.github.com>
2021-04-08 19:52:15 +0300

    Update Main.java

--- 06.04.21/Main.java
+++ 06.04.21/Main.java
@@ -1,24 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        Person ira = new Person("Ирина",75,null,null);
-        Person kolya = new Person("Николай",77,null,null);
-        Person petr = new Person("Петр",73,null,null);
-        Person katya= new Person("Екатерина",70,null,null);
-        Person oleg = new Person("Олег",45,ira,kolya);
-        Person olga = new Person("Ольга",47, katya,petr);
-        Person igor = new Person("Игорь",20, olga,oleg);
+        Person ira = new Person("Ирина", 75, null, null);
+        Person kolya = new Person("Николай", 77, null, null);
+        Person petr = new Person("Петр", 73, null, null);
+        Person katya = new Person("Екатерина", 70, null, null);
+        Person oleg = new Person("Олег", 45, ira, kolya);
+        Person olga = new Person("Ольга", 47, katya, petr);
+        Person igor = new Person("Игорь", 20, olga, oleg);
 
         //System.out.println(igor.getMother().getName());
         igor.getInfo();
     }
 }
+
 // У человека не может бытьь более 100 едениц здоровья
-class Person{
+class Person {
     private String name;
     private int age;
     private int hp;
     private Person mother;
     private Person father;
+
     public Person(String name, int age, Person mother, Person father) {
         this.name = name;
         this.age = age;
@@ -26,6 +28,7 @@ class Person{
         this.mother = mother;
         this.father = father;
     }
+
     public Person getMother() {
         return mother;
     }
@@ -33,11 +36,15 @@ class Person{
     public Person getFather() {
         return father;
     }
-    void setHp(int hp){
-        if(this.hp+hp>=100) this.hp = 100;
+
+    void setHp(int hp) {
+        if (this.hp + hp >= 100) this.hp = 100;
         else this.hp += hp;
     }
-    int getHp(){return this.hp;}
+
+    int getHp() {
+        return this.hp;
+    }
 
     public String getName() {
         return name;
@@ -55,22 +62,24 @@ class Person{
         this.age = age;
     }
 
-    public void getInfo(){
+    public void getInfo() {
         String info = "";
-        info += "Меня зовут "+this.name+"\n"+
-                "Мне "+this.age+" лет \n";
-        if(this.mother != null){
+        info += "Меня зовут " + this.name + "\n" +
+                "Мне " + this.age + " лет \n";
+        if (this.mother != null) {
             info += "Мою маму зовут " + this.mother.name + "\n";
-            if(this.mother.mother != null)
-                info += "Бабушку по маминой линии зовут " + this.mother.mother.name + "\n";
-                if(this.mother.father !=null)
-                info += "Дедушку по маминой линии зовут " + this.mother.father.name+ "\n";
+        if (this.mother.mother != null)
+            info += "Бабушку по маминой линии зовут " + this.mother.mother.name + "\n";
+        if (this.mother.father != null)
+            info += "Дедушку по маминой линии зовут " + this.mother.father.name + "\n";
         }
-        if(this.father != null) info += "Моего папу зовут "+this.father.name+"\n";
-        if(this.father.father !=null)
-        info += "Дедушку по папиной линии зовут "+this.father.father.name+"\n";
-        if(this.father.mother !=null)
-        info += "Бабушку по папиной линии зовут "+this.father.mother.name+"\n";
-        System.out.println(info);
+        if (this.father != null) {
+            info += "Моего папу зовут " + this.father.name + "\n";
+        if (this.father.father != null)
+            info += "Дедушку по папиной линии зовут " + this.father.father.name + "\n";
+        if (this.father.mother != null)
+            info += "Бабушку по папиной линии зовут " + this.father.mother.name + "\n";
+            System.out.println(info);
+        }
     }
 }
