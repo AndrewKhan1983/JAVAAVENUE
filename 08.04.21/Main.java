128b1f08e72f5482daa1c826620e27f4d612fda4
Андрей Хан <81438426+AndrewKhan1983@users.noreply.github.com>
2021-04-12 20:24:38 +0300

    Create Main.java

---
+++ 08.04.21/Main.java
@@ -0,0 +1,24 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Family_Tree {
+    public static void main(String[] args) {
+        try {
+            FileReader reader = new FileReader("C:\\java\\test.txt");
+            int i;
+            String ip = "";
+            String port = "";
+            while ((i = reader.read()) != -1)
+
+
+            System.out.println("IP: "+ip+" port: "+port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
