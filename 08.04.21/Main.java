c25463797a5d4e77e886469a5c792a05e8b79510
Андрей Хан <81438426+AndrewKhan1983@users.noreply.github.com>
2021-04-12 21:09:13 +0300

    Update Main.java

--- 08.04.21/Main.java
+++ 08.04.21/Main.java
@@ -8,13 +8,16 @@ public class Family_Tree {
     public static void main(String[] args) {
         try {
             FileReader reader = new FileReader("C:\\java\\test.txt");
-            int i ;
+            int i;
             String ip = "";
             String port = "";
             while ((i = reader.read()) != -1)
-           System.out.print((char) i);
+
+                System.out.print((char) i);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
     }
 }
