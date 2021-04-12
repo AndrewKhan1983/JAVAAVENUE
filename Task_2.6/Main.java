b12df5204c29d2b67287c2e63aa6cbe001eb076f
Андрей Хан <81438426+AndrewKhan1983@users.noreply.github.com>
2021-04-12 21:27:40 +0300

    Create Main.java

---
+++ Task_2.6/Main.java
@@ -0,0 +1,11 @@
+public class Family_Tree {
+    public static void main(String[] args) {
+        int a = 30;
+        int b = 10;
+        int z = a%b;
+        if (z == 0) System.out.println("Делится " +z);
+        else System.out.println("Не делится " +z );
+
+
+    }
+}
