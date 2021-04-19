30b497b7b7b30b333d24900c7d8165e4ff2b5c8a
Андрей Хан <81438426+AndrewKhan1983@users.noreply.github.com>
2021-04-19 21:58:38 +0300

    Create Main.java

---
+++ task_2.9/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int mass[] = {2,3,4,5,7,111};
+        for (int i = 0; i < mass.length; i++) {
+            if (mass[i] == 5){
+                System.out.println("Да ");
+                }
+            else if(mass [i] != 5){
+                System.out.println("Нет ");
+            }
+        }
+
+    }
+}
